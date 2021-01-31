18b642f11221cc0e458743fb1dc5f3650e486d74
Dmitriy <d29bondarenko@gmail.com>
2021-01-31 20:22:06 +0700

    Реализовано получение данных с сервера с помощью fetch, данные подставлены в каждую пиццу

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Route } from 'react-router-dom';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((resp) => resp.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []);
+
+  console.log(pizzas);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content"></div>
-      <Route path="/" component={Home} exact />
+      <Route path="/" render={() => <Home items={pizzas} />} exact />
       <Route path="/cart" component={Cart} exact />
     </div>
   );
